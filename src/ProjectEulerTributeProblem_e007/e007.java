49895567bf891f3d4b5b7185f7e5940522b66cec
Bill Nicholson <nicholdw@ucmail.uc.edu>
2016-04-22 16:37:02 -0400

    Initial Commit

---
+++ src/ProjectEulerTributeProblem_e007/e007.java
@@ -0,0 +1,24 @@
+/*
+ * Two numbers are coprime if they have no common factors except 1. 
+ * The totient φ(n) of a positive integer n greater than 1 is defined to be the number of positive integers less than n that are coprime to n.
+ * 
+ * Compute the 
+ */
+
+package ProjectEulerTributeProblem_e007;
+
+public class e007 {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public static boolean isCoprime(int a, int b) {
+		boolean result = true;
+		
+		
+		return result;
+	}
+
+}
